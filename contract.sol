f83d010ef33e6ed1793f5df555eb524fe247b6c2
bnthck <62210684+bnthck@users.noreply.github.com>
2021-11-24 16:55:34 +0800

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,49 +1,49 @@
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract pledge {
-    
-    
+    using SafeMathChainlink for uint256; // I don't know why but it sounds nice 
     bool public date_reached=false;
     bool public steps_reached=false;
     uint256 public steps_target=100000;
     uint256 public date_target=20210101;
     uint256 private correct_password = 42;
+    uint256 public minimumUSD = 50 * 10 ** 18; //require at least 50 usd in stupid units
     
-    //this tracks who sent how much
+    uint8 public max_donors = 20; // max number of donors so dont risk gas getting too expensive
+    uint8 public donors_so_far = 0;
     
     // a new type to contain all relevant info about a depoit to the contract
     struct deposit{
         uint256 amount;
-        address success_destination;
-        address fail_destination;
+        address payable success_destination;
+        address payable fail_destination;
     }
-    
     mapping (address=>deposit) public address_to_deposit;
     address[] public userAddresses; // this is needed to iterate the mapping. mappings alone cannot be iterated
     
-    
-    //To withdraw a token balance, you need to execute the transfer() function on the token contract. So in order to withdraw all tokens, you need to execute the transfer() function on all token contracts.
-    //You can create a function that withdraws any ERC-20 token based on the token contract address that you pass as an input.
-    
-    //pragma solidity ^0.8;
-    
-    // what is external keyword - changed to internal - this is only to be called by this contract itself
-    // external is the same as public, but to be used when you will never call it internally. due to some technical details
-    // public functions use more gas than external
-    // what is an interface - i forgot
-    interface IERC20 {
-        function transfer(address _to, uint256 _amount) internal returns (bool);
+
+        //Not sure if/why we need this
+        function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
     }
     
-    // Figure out exactly how this is called
-    function fund() external payable{
-        //There needs to be a limit to the ammoung of people thaat can fund as iterating through them uses gas and there is a block gas limit
-        // could end up with a contract that would require too much gas to excecute
-        address_to_deposit[msg.sender].amount += msg.value; /// Will the msg indicate what token too, do we need a dif function for every ERC-20
-        userAddresses.push(msg.sender); //need an array to record these to iterate the mapping
-        
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+         return uint256(answer * 10000000000);
     }
     
+    // this converts however much eth they spend to usd
+    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+    }
     
     //// These query functions will not be 'view' they will require transactions to the oracles
     function query_date() public view returns(uint256){
@@ -55,35 +55,69 @@ contract pledge {
         return 100000;
     }
     
+    
+    constructor(uint256 _steps_target, uint256 _date_target) public payable{
+        //owner = msg.sender; // What does this do?
+        
+        require(_steps_target > 100,"have some ambition dickhead");
+        require(_date_target > 20211124, "that's in the past you twat" ); // Could query todays date here if it were worth the money to bother
+        
+        steps_target= _steps_target;
+        date_target = _date_target;
+        
+        // code below same as fund() - see there for comments
+        require(getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
+        address_to_deposit[msg.sender].amount += msg.value; 
+        userAddresses.push(msg.sender); 
+    }
+    function fund() external payable{
+        require(donors_so_far<max_donors, "Fuck off we're full");
+        require(getConversionRate(msg.value) >= minimumUSD, "comeon cheapskate");
+        address_to_deposit[msg.sender].amount += msg.value; /// Will the msg indicate what token too, do we need a dif function for every ERC-20
+        userAddresses.push(msg.sender); //need an array to record these to iterate the mapping
+    }
+    
     //This is internal because nobody should have the power to distribute the eth other than the 'check' function
-    function distribute_eth(bool steps_reached) internal{
+    function distribute_eth(bool _steps_reached) internal{
         //here goes the code to send the eth to the charity or the user depending if steps_reached is true or false
-        if (steps_reached){
+        if (_steps_reached){
                 //for every deposit, transfer to success_destination
-                for (uint i=0; i<studentList.length; i++) {
-                    transfer( address_to_deposit[i].success_destination, address_to_deposit[i].amount) 
+                for (uint i=0; i<userAddresses.length; i++) {
+                    address add=userAddresses[i];
+                    transfer( address_to_deposit[add].success_destination, address_to_deposit[add].amount); 
             }
         }
         else{
                 // for every deposit, transfer to fail destination
-                for (uint i=0; i<studentList.length; i++) {
-                    transfer( address_to_deposit[i].fail_destination, address_to_deposit[i].amount) 
+                for (uint i=0; i<userAddresses.length; i++) {
+                    address add=userAddresses[i];
+                    transfer( address_to_deposit[add].fail_destination, address_to_deposit[add].amount);
             }
         }
     }
     
+    
+    function transfer(address payable to, uint256 amount) internal returns(bool){
+        ///require(msg.sender==oracle address); no doing this elsewhere and with password
+        to.transfer(amount);
+    }
+    
+    
+    
     // function for keeper to trigger which will check date and steps and distribute eth if appropriate
     function check(uint256 password) public returns(bool){
         
+        //require(msg.sender==oracle address) ----------------------- do we want this? perhaps anyone should be able to trigger a check
+        // Perhaps dont want the password either
         if (password != correct_password){
             return false;
         }
         
-        date = query_date();
-        steps = query_steps();
+        uint256 date = query_date();
+        uint256 steps = query_steps();
         
         if (date >= date_target){
-            data_reached=true;
+            date_reached=true;
         }
         if (steps >= steps_target){
             steps_reached=true;
@@ -101,3 +135,9 @@ contract pledge {
         }
         
     }
+    
+
+    
+    
+    
+}
